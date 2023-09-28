a8c55845d8107a3a476e3a7966dbf20cb620e7fa
Abhinavgundapaneni <abhinvgundapaneni@gmail.com>
2023-09-29 01:08:41 +0530

    Email template changes

--- DataAccess/Helper.cs
+++ DataAccess/Helper.cs
@@ -10,10 +10,13 @@ namespace DataToolKit.DataAccess
     public class Helper
     {
         private IConfiguration? _configuration { get; set; }
+        private DTKDB _dtkdb {  get; set; }
 
         public Helper(IConfiguration? configuration)
         {
             _configuration = configuration ?? null;
+            _dtkdb = new DTKDB(configuration);
+
         }
         public bool isLoggedInUserAdmin(ClaimsPrincipal user)
         {
@@ -30,6 +33,12 @@ namespace DataToolKit.DataAccess
         {
             var emailClient = new EmailClient(_configuration?.GetConnectionString("EmailCommunicationServiceConnection"));
             var recipientList = _configuration?.GetSection("EmailService:ReciepientAddressList").Get<List<string>>();
+            var batchControl = _dtkdb.GetBatchControls().Where(x => x.BatchId == batchId).ToList();
+            if(batchControl.Count < 1)
+            {
+                // No Batch Id Found, Ideally should never happen.
+                return;
+            }
             foreach(var recipient in recipientList)
             {
                 await emailClient.SendAsync(
@@ -39,10 +48,18 @@ namespace DataToolKit.DataAccess
                 subject: _configuration?.GetSection("EmailService:EmailSubject").Get<string>(),
                 htmlContent: @"
                             <html>
-                                Hello,<br>
-                                <p>New File uploaded by user.<br><br>
-                                Batch Id: " + batchId.ToString() + @"<br></p>
-                                Thanks
+                                Hello,<br><br>
+                                <p> We are pleased to inform you that a new file has been successfully uploaded to the Data Tool Kit application.<br><br>
+                                File Details:<br><br>
+                                Customer: " + batchControl[0].CustomerName + @"<br>
+                                File Name: " + batchControl[0].InputFileName + @"<br>
+                                Report: " + batchControl[0].ReportTitle + @"<br>
+                                Uploaded By: " + batchControl[0].SubmitName + @"<br>
+                                Batch Id: " + batchId.ToString() + @"<br><br> 
+                                You can access this file by logging into your https://dtkpocn.azurewebsites.net/ account.<br><br></p>
+                                Best regards<br>
+                                POCN<br>
+                                Data Tool Kit application.
                             </ html >");
             }
         }
